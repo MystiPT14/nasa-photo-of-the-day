8b9e3e60ff30a0a539794db32e8017583db8760d
Mysti Dyse <mystidyse@gmail.com>
2020-03-05 16:21:44 -0800

    added component folder and imported them to app.js included state in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import Header from "./components/header";
+import Main from "./components/main";
+import Footer from "./components/footer";
+
+
+
 
 function App() {
+
+  const [photo, setphoto] = useState('')
   return (
     <div className="App">
       <p>
